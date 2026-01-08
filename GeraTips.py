8c3f47a9c690486139f5754acd9eed3074be78c4
vinicmiranda <74029753+vinicmiranda@users.noreply.github.com>
2026-01-08 10:59:50 -0300

    Include 'Div' column in printed DataFrame outputs

--- GeraTips.py
+++ GeraTips.py
@@ -202,7 +202,7 @@ def baixar_jogos_extra(destino):
     df.columns = ['Div','Data','Hora','Mandante','Visitante']
     df.to_excel(destino, index=False)
     print("📄 Jogos EXTRA baixados:")
-    print(df[['Data','Hora','Mandante','Visitante']])
+    print(df[['Div','Data','Hora','Mandante','Visitante']])
 
 def baixar_jogos_main(destino):
     soup = BeautifulSoup(
@@ -217,7 +217,7 @@ def baixar_jogos_main(destino):
     df.columns = ['Div','Data','Hora','Mandante','Visitante']
     df.to_excel(destino, index=False)
     print("📄 Jogos MAIN baixados:")
-    print(df[['Data','Hora','Mandante','Visitante']])
+    print(df[['Div','Data','Hora','Mandante','Visitante']])
 
 def baixar_temp_extra(destino_dir):
     soup = BeautifulSoup(
